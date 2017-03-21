74bc419e9f124225120fd21f8c79f6b5410702d6
RDF014 <rdf014@gmail.com>
2017-03-21 04:07:26 -0700

    [Feature] Able to transcribe example audio. Researching proper recording and how to stream that data back to user

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,12 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var request = require('request');
 var db = require('../mongo-db/config.js');
 var inputs = require('../mongo-db/inputs.js');
-var fs = require('fs');
-// var Speech = require('../Server/speechToText.js')
+var Speech = require('../Server/speechToText.js')
+
+var app = express();
 
 app.use(express.static(__dirname + '/../angular-client'));
 app.use(express.static(__dirname + '/../node_modules'));
@@ -12,60 +14,15 @@ app.use(bodyParser.json({
     extended: true
 }));
 
-
 var port = process.env.PORT || 5000;
-
 
 app.post('/log', function(req, res) {
   console.log('req.body.query', req.body.query);
-  
-  let speech = require('@google-cloud/speech')({
-    projectId: 'Isolineator',
-    keyFilename: './Server/SpeechToText/isolineator-a25b826f81b6.json'
-  });
-
-  // var request = {
-  //   config: {
-  //     encoding: 'LINEAR16',
-  //     sampleRate: 16000
-  //   },
-  //   singleUtterance: false,
-  //   interimResults: false
-  // };
-
-  // fs.createReadStream('./Test.wav')
-  //   .on('error', console.error)
-  //   .pipe(speech.createRecognizeStream(request))
-  //   .on('error', console.error)
-  //   .on('data', function(data) {
-  //     console.log(data);
-  //   });
-
-  // const recognizeStream = speech.createRecognizeStream(request)
-  //   .on('error', console.error)
-  //   .on('data', (data) => {
-  //     console.log('Data received: ', data);
-  //   });
-  // fs.createReadStream('./audio.raw').pipe(recognizeStream);
-
-  // res.status(201).send();
-  
-  let options = {
-    encoding: 'LINEAR16',
-    sampleRate: 16000
-  };
-  
-  speech.recognize('./Server/Test.wav', options)
-  .then((results) => {
-    const transcription = results[0];
-    console.log(`Transcription: ${transcription}`);
+  Speech.streamAudio('./audio.raw', (data)=>{
+    if(data.endpointerType === 'ENDPOINTER_EVENT_UNSPECIFIED') {
+      res.status(200).send(data.results);
+    }
   })
-  .then((data) => {
-    res.status(201).send();
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 });
 
 app.post('/', function(req, res) {
--- Server/speechToText.js
+++ Server/speechToText.js
@@ -1,62 +1,60 @@
-// var google = require('googleapis');
+const fs = require('fs');
 
-// // This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS
-// // environment variables.
-// google.auth.getApplicationDefault(function (err, authClient, projectId) {
-//   if (err) {
-//     throw err;
-//   }
-
-//   // The createScopedRequired method returns true when running on GAE or a local developer
-//   // machine. In that case, the desired scopes must be passed in manually. When the code is
-//   // running in GCE or a Managed VM, the scopes are pulled from the GCE metadata server.
-//   // See https://cloud.google.com/compute/docs/authentication for more information.
-//   if (authClient.createScopedRequired && authClient.createScopedRequired()) {
-//     // Scopes can be specified either as an array or as a single, space-delimited string.
-//     authClient = authClient.createScoped([
-//       'https://www.googleapis.com/auth/compute'
-//     ]);
-//   }
-
-//   // Fetch the list of GCE zones within a project.
-//   // NOTE: You must fill in your valid project ID before running this sample!
-//   var compute = google.compute({
-//     version: 'v1',
-//     auth: authClient
-//   });
-  
-//   var projectId = 'YOUR_PROJECT_ID';
-
-//   compute.zones.list({
-//     project: projectId,
-//     auth: authClient
-//   }, function (err, result) {
-//     console.log(err, result);
-//   });
-// });
-
-const Speech = require('@google-cloud/speech');
-
-// Your Google Cloud Platform project ID
-const projectId = 'Isolineator';
-
-// Instantiates a client
-exports.speechClient = Speech({
-  projectId: projectId
+const Speech = require('@google-cloud/speech')({
+  projectId: 'Isolineator',
+  keyFilename: './Server/SpeechToText/isolineator-a25b826f81b6.json'
 });
 
-// The name of the audio file to transcribe
 const fileName = './Test.m4a';
 
-// The audio file's encoding and sample rate
 const options = {
-  encoding: 'm4a',
+  encoding: 'LINEAR16',
   sampleRate: 16000
 };
 
-// Detects speech in the audio file
-// speechClient.recognize(fileName, options)
-//   .then((results) => {
-//     const transcription = results[0];
-//     console.log(`Transcription: ${transcription}`);
-//   });
+const request = {
+  config: {
+    encoding: 'LINEAR16',
+    sampleRate: 16000
+  },
+  singleUtterance: false,
+  interimResults: false
+};
+//################STREAMING AUDIO###################
+  exports.streamAudio = (file, callback) => {
+    fs.createReadStream(file)
+    .on('error', console.error)
+    .pipe(Speech.createRecognizeStream(request))
+    .on('error', console.error)
+    .on('data', function(data) {
+      console.log(data);
+      callback(data)
+    })
+  };
+//////////////Same functions///////////////
+  // const recognizeStream = speech.createRecognizeStream(request)
+  //   .on('error', console.error)
+  //   .on('data', (data) => {
+  //     console.log('Data received: ', data);
+  //   });
+  // fs.createReadStream('./audio.raw').pipe(recognizeStream);
+
+  // res.status(201).send();
+//#######################################
+
+//################normal synchronus####################
+  exports.syncAudio = (file, callback) => {
+    Speech.recognize(file, options)
+    .then((results) => {
+      const transcription = results[0];
+      console.log(`Transcription: ${transcription}`);
+      return transcription; 
+    })
+    .then((data) => {
+      callback(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+//##########################################
