dbe89cf47009fbd70ec42598cef928d0b1f74bb9
Christopher Yang <chrisyang190@gmail.com>
2017-03-21 01:56:31 -0700

    Initial Recording Functionality Implemented

--- angular-client/index.js
+++ angular-client/index.js
@@ -1,1 +1,13 @@
-angular.module('app', []);
+angular.module('app', ['angularAudioRecorder'])
+    .controller('DemoController', function ($scope, $timeout) {
+      console.log('Loaded');
+      $scope.timeLimit = 10;
+
+
+    }).config(function (recorderServiceProvider) {
+      recorderServiceProvider
+      .forceSwf(false)
+      //.setSwfUrl('/lib/recorder.swf')
+      .withMp3Conversion(true)
+    ;
+    });
