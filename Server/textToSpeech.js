899280ab45a07d875ced7aea0d00eec9d7918c43
jdbriner07 <jdbriner07@gmail.com>
2017-03-28 11:22:07 -0700

    commented out files requiring api key files

--- Server/textToSpeech.js
+++ Server/textToSpeech.js
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 const Stream = require('stream');
 const Voices = require('./voices.js');
 
-AWS.config.loadFromPath('./APIs/isolineatorCreds.json');
+// AWS.config.loadFromPath('./APIs/isolineatorCreds.json');
 
 var polly = new AWS.Polly();
 
