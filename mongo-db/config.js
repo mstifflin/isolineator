17a44f3107c7817a8c878c87fe935b1faaa2d490
jdbriner07 <jdbriner07@gmail.com>
2017-03-28 11:48:37 -0700

    configuring to deployed database

--- mongo-db/config.js
+++ mongo-db/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
-var mongoURI = 'mongodb://localhost/isodb';
+var mongoURI = process.env.DB_URL || 'mongodb://localhost/isodb';
 mongoose.connect(mongoURI);
 
 var dbconn = mongoose.connection;
@@ -10,3 +10,5 @@ dbconn.on('open', function () {
 });
 
 module.exports.dbconn = dbconn;
+
+
