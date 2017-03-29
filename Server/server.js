9be5a09f2b286ca7f014675f1ed72805d16d6284
lg_baum <laura.greenbaum@gmail.com>
2017-03-29 12:17:28 -0700

    Dropdown selects the language you want to translate to and view messages in.

--- Server/server.js
+++ Server/server.js
@@ -17,7 +17,6 @@ const io = require ('socket.io')(server);
   
 io.on('connection', (socket) => {
  socket.on('message', (message) => {
-   console.log(message, "this is a message");
    io.emit('message', message);
  })
 
